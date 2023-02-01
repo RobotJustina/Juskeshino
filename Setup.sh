fc137c30a7f52b287889dfb785e1af349d1cc1ad
Lap Jusina <generic@generic.com>
2023-02-01 16:09:51 -0600

    first version of setup.sh add file for udev rules

---
+++ Setup.sh
@@ -0,0 +1,5 @@
+sudo apt install -y ros-noetic-dynamixel-sdk ros-noetic-serial ros-noetic-random-numbers ros-noetic-map-server ros-noetic-amcl ros-noetic-sound-play ros-noetic-effort-controllers ros-noetic-controller-manager ros-noetic-urg-node portaudio19-dev python3-pip festvox-us-slt-hts
+sudo pip3 install pyaudio
+sudo pip3 install swig
+sudo pip3 install pocketsphinx==0.1.15
+sudo pip3 install urdf-parser-py
