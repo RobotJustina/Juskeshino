a26b2aa02495a61a703baac5729e6e7c58e0c597
sergioar3134 <sergioar3134@github.com>
2023-02-20 11:47:07 -0800

    Se cambiaron los nombres, solo funciona para obstaculos en la izquierda

--- catkin_ws/src/vision/machine_learning/src/RL.py
+++ catkin_ws/src/vision/machine_learning/src/RL.py
@@ -1,70 +1,44 @@
 #! /usr/bin/env python3
 #
-# Position 3.2597, 6.2913, 0.13
-# Orientacion 0,0,-0.707,0.707
-
-#  z=-0.13
-# Pos  4.73, 6.13, 0.13
-# Ori 0,0, -0.62, 0.78
-
-# z=-0.145
-# Pos 4.83 6.09 0.13
-#Ori 0 0 -0.72 0.69
-
-### mover simple_move/goal_dist_lateral
-
+### mover izquiera o derecha simple_move/goal_dist_lateral
+### mover al frente simple_move/goal_dist
 import rospy
 import numpy as np
 from sensor_msgs.msg   import LaserScan
-from geometry_msgs.msg import Twist
-
+from std_msgs.msg import Float32
+from std_msgs.msg import Empty
 
 def Last_pub():
-    pub_cmd_vel.publish(Twist())
+    pub_stop.publish(Empty())
     print("Se cierra el nodo")
 
 def callback_laser_scan(msg):
-    #
-    # TODO:
-    # Do something to detect if there is an obstacle in front of the robot.
-    #
-    global obstacle
-    #obstacle=msg.ranges[len(msg.ranges)//2]<1.0 185
-    #np.random.randint(0, len(msg))
-    obstacle=msg.ranges[len(msg.ranges)-1]<1.0
-    print(len(msg.ranges))
+    global obstacle_left
+    obstacle_left=False
+    for i in range(154,184):
+        temp=msg.ranges[i]<1.0
+        obstacle_left=obstacle_left or temp
     return
 
 rospy.init_node("RL")
 rospy.Subscriber("/hardware/scan", LaserScan, callback_laser_scan)
-pub_cmd_vel = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
+pub_lat = rospy.Publisher("/simple_move/goal_dist_lateral", Float32, queue_size=10)
+pub_fro = rospy.Publisher("/simple_move/goal_dist", Float32, queue_size=10)
+pub_stop = rospy.Publisher("/simple_move/stop", Empty, queue_size=10)
 loop = rospy.Rate(10)
 rospy.on_shutdown(Last_pub)
 
 
 def main():
-    global obstacle
-    #rospy.init_node("RL")
-    #rospy.Subscriber("/hardware/scan", LaserScan, callback_laser_scan)
-    #pub_cmd_vel = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
-    #loop = rospy.Rate(10)
-    #rospy.on_shutdown(Last_pub)
-
-    obstacle=False
-    cmd_vel=Twist()
+    global obstacle_left
+    obstacle_left=False
+    goal_lateral=Float32()
     while True:
-        #
-        # TODO:
-        # Declare a Twist message and assign the appropiate speeds:
-        # Move forward if there is no obstacle in front and stop otherwise.
-        # Publish the message.
-        #
-        if obstacle:
-            cmd_vel=Twist()
+        if obstacle_left:
+            pub_stop.publish(Empty())
         else:
-            cmd_vel.linear.y=0.2
-            cmd_vel.angular.z=-0.13
-        pub_cmd_vel.publish(cmd_vel)
+            goal_lateral.data=0.2
+            pub_lat.publish(goal_lateral)
         loop.sleep()
 
 if __name__ == '__main__':
