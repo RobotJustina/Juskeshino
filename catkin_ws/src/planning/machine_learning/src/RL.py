2bfb544e53ddc7678e9c1abf5f55db7b7cedbf4e
sergioar3134 <sergioar3134@github.com>
2023-02-22 10:02:43 -0800

    Se cambiaron los directorios y se creo la primera versión de RL

--- catkin_ws/src/planning/machine_learning/src/RL.py
+++ catkin_ws/src/planning/machine_learning/src/RL.py
@@ -15,7 +15,6 @@ def Last_pub():
     return
 
 def callback_goal(msg):
-    print("Se llego a la meta")
     global next
     if(msg.status==3):
         next=True
@@ -27,13 +26,13 @@ def callback_laser_scan(msg):
     obstacle_left=False
     obstacle_right=False
     obstacle=False
-    for i in range(154,184):
+    for i in range(152,184):
         temp=msg.ranges[i]<1.0
         obstacle_left=obstacle_left or temp
-    for i in range(0,30):
+    for i in range(0,32):
        temp=msg.ranges[i]<1.0
        obstacle_right=obstacle_right or temp
-    for i in range(77,107):
+    for i in range(74,110):
        temp=msg.ranges[i]<1.0
        obstacle=obstacle or temp
     if not(obstacle)  and not(obstacle_left) and not(obstacle_right):
@@ -58,24 +57,16 @@ def do_action(act):
     goal_lateral=Float32()
     goal=Float32()
     if (act==0): #ir hacia el frente
-         goal.data=0.2
-         goal_lateral=0.0
-         pub_lat.publish(goal_lateral)
+         goal.data=0.1
          pub_fro.publish(goal)
     elif(act==1): #ir a la izquierda
-         goal_lateral.data=0.2
-         goal.data=0.0
-         pub_fro.publish(goal)
+         goal_lateral.data=0.1
          pub_lat.publish(goal_lateral)
     elif(act==2): #ir  a la derecha
-         goal.data=0.0
-         goal_lateral=-0.2
-         pub_fro.publish(goal)
+         goal_lateral=-0.1
          pub_lat.publish(goal_lateral)
     else: ###ir hacia atrás
-         goal.data=-0.2
-         goal_lateral=0.0
-         pub_lat.publish(goal_lateral)
+         goal.data=-0.1
          pub_fro.publish(goal)
     return
 
@@ -85,40 +76,46 @@ rospy.Subscriber("/simple_move/goal_reached", GoalStatus, callback_goal)
 pub_lat = rospy.Publisher("/simple_move/goal_dist_lateral", Float32, queue_size=10)
 pub_fro = rospy.Publisher("/simple_move/goal_dist", Float32, queue_size=10)
 pub_stop = rospy.Publisher("/simple_move/stop", Empty, queue_size=10)
-#loop = rospy.Rate(1)
+loop = rospy.Rate(0.5)
 rospy.on_shutdown(Last_pub)
 
 def Q_values():
     Q=np.zeros((8,4))
-    Q[0,0]=9.83783079
-    Q[0,1]=-0.52186371
-    Q[0,2]=-6.86721928
-    Q[0,3]=-15.20510166
+    Q[0,0]=5.99900391
+    Q[0,1]=-1.90608052
+    Q[0,2]=1.32473345
+    Q[0,3]=-1.82684756
 
-    Q[1,0]=13.54966589
-    Q[1,1]=45.27875365
-    Q[1,2]=45.58711282
-    Q[1,3]=-8.99421424
+    Q[1,0]=-5.92838938
+    Q[1,1]=33.53236352
+    Q[1,2]=15.69608459
+    Q[1,3]=-3.81752493
 
-    Q[2,0]=28.00631636
-    Q[2,1]=-57.85837207
-    Q[2,2]=-1.63933125
-    Q[2,3]=-0.85
+    Q[2,0]=0
+    Q[2,1]=0
+    Q[2,2]=0
+    Q[2,3]=0
 
-    Q[3,0]=11.11689248
-    Q[3,1]=10.87116953
-    Q[3,2]=-5.96812786
-    Q[3,3]=-0.9570219
+    Q[3,0]=31.7809589
+    Q[3,1]=4.30146037
+    Q[3,2]=-5.5936299
+    Q[3,3]=1.59059546
 
-    Q[4,0]=-13.57794324
-    Q[4,1]=-38.29580639
-    Q[4,2]=-32.32615104
-    Q[4,3]=-33.31341905
+    Q[4,0]=0
+    Q[4,1]=0
+    Q[4,2]=0
+    Q[4,3]=0
 
-    Q[5,0]=-5.2259098
-    Q[5,1]=42.6387361
-    Q[5,2]=23.00825518
-    Q[5,3]=15.42163419
+    Q[5,0]=-9.64333788
+    Q[5,1]=8.28557938
+    Q[5,2]=0
+    Q[5,3]=10.11073587
+
+    Q[7,0]=-22.8296999
+    Q[7,1]=-10.69824534
+    Q[7,2]=0
+    Q[7,3]=0
+
     return Q
 
 def R_values():
@@ -186,29 +183,40 @@ def main():
     R=R_values()
     epsilon = 0.9
     total_episodes = 10000
-    max_steps = 100
+    max_steps = 20
     alpha = 0.85
     gamma = 0.95
     #do_action(act)
+    steps=-4
+    while steps <0:
+        steps=steps+1
+        prueba=Float32()
+        prueba.data=-0.03
+        pub_fro.publish(prueba)
+        loop.sleep()
+
     for x in range(total_episodes):
-        ##Inicializar
+        if(rospy.is_shutdown()):
+            break
         G=0
         act_ant=choose_action(edo)
         edo_ant=edo
+        next=False
         steps=0
-        while steps<max_steps:
+        while steps<max_steps and not(rospy.is_shutdown()):
             #Se realiza la accion anterior y se escoge una nueva dependiendo del estado
+            #do_action(act_ant)
             do_action(act_ant)
-            next=False
-            #do_action(0)
+            while(not(next) and not(rospy.is_shutdown())):
+                pass
+                #do_action(act_ant)
+                #loop.sleep()
             act=choose_action(edo)
+            next=False
             Q[edo_ant,act_ant]=Q[edo_ant,act_ant]+alpha*(R[edo_ant,act_ant]+gamma*Q[edo,act]-Q[edo_ant,act_ant])
             G=G+R[edo_ant,act_ant]
             edo_ant, act_ant = edo, act
             steps=steps+1
-            #loop.sleep()
-            while(not(next)):
-                print(steps)
         print("Episodio "+str(x)+" Ganancia total "+str(G))
         print(Q)
 
