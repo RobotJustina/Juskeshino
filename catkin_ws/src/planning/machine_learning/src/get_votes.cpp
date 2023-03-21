fc638747de0f155d2af947b0089853d50f6132bb
sergioar3143 <sergioar3143@github.com>
2023-03-20 18:27:59 -0600

    RL.py listens topic votes and saves matrix Q, get_votes counts votes in the first for cycle

--- catkin_ws/src/planning/machine_learning/src/get_votes.cpp
+++ catkin_ws/src/planning/machine_learning/src/get_votes.cpp
@@ -5,6 +5,7 @@
 #include "cv_bridge/cv_bridge.h"
 #include "opencv2/opencv.hpp"
 #include "std_msgs/Float64MultiArray.h"
+#include "std_msgs/Int32MultiArray.h"
 
 tf::TransformListener* tf_listener;
 //sensor_msgs::PointCloud2::Ptr point_cloud_ptr2;
@@ -13,12 +14,13 @@ cv::Mat bgr_dest;
 cv::Mat pc_dest;
 int info=0;
 //int votes[3][3]={};
+ros::Publisher pub_votes;
 
 void callback_pointcloud(sensor_msgs::PointCloud2 msg){
     int votes[3][3]={};
-    float xmin=-0.699;
-    float ymin=-1.499;
-    float d=1;
+    float xmin=-0.549;
+    float ymin=-1.199;
+    float d=0.8;
     pcl_ros::transformPointCloud("base_link", msg, pc_msg, *tf_listener);
     int offset_x = 0;
     int offset_y = 4;
@@ -49,15 +51,22 @@ void callback_pointcloud(sensor_msgs::PointCloud2 msg){
             bgr_dest.data[j*bgr_dest.step + i*3]     = pc_msg.data[(j*pc_msg.width + i)*pc_msg.point_step + offset_bgr];
             bgr_dest.data[j*bgr_dest.step + i*3 + 1] = pc_msg.data[(j*pc_msg.width + i)*pc_msg.point_step + offset_bgr + 1];
             bgr_dest.data[j*bgr_dest.step + i*3 + 2] = pc_msg.data[(j*pc_msg.width + i)*pc_msg.point_step + offset_bgr + 2];
-        }
-    for(size_t i=0; i < bgr_dest.cols; i++)
-        for(size_t j=0; j < bgr_dest.rows; j++){
+            //next lines get the votes in order to know the robot state
             int r= (pc_dest.at<cv::Vec3f>(j, i)[0]-xmin)/d;
             int c= (pc_dest.at<cv::Vec3f>(j, i)[1]-ymin)/d;
             float h= pc_dest.at<cv::Vec3f>(j, i)[2];
             if(r<=2 && r>=0 && c<=2 && c>=0 && h>0.2)
                 votes[r][int(2-c)]+=1;
-    }
+
+        }
+    //for(size_t i=0; i < bgr_dest.cols; i++)
+        //for(size_t j=0; j < bgr_dest.rows; j++){
+            //int r= (pc_dest.at<cv::Vec3f>(j, i)[0]-xmin)/d;
+            //int c= (pc_dest.at<cv::Vec3f>(j, i)[1]-ymin)/d;
+            //float h= pc_dest.at<cv::Vec3f>(j, i)[2];
+            //if(r<=2 && r>=0 && c<=2 && c>=0 && h>0.2)
+                //votes[r][int(2-c)]+=1;
+    //}
     info=1;
     for(int i=0; i<=2; i++)
         for(int j=0; j<=2; j++){
@@ -66,7 +75,15 @@ void callback_pointcloud(sensor_msgs::PointCloud2 msg){
             else
                 votes[i][j]=0;
     }
-    std::cout<<votes[0][0]<<" "<<votes[0][1]<<" "<<votes[0][2]<<" "<<votes[1][0]<<" "<<votes[1][1]<<" "<<votes[1][2]<<std::endl;
+    std_msgs::Int32MultiArray msg_votes;
+    msg_votes.data.resize(6);
+    msg_votes.data[0] = votes[0][0];
+    msg_votes.data[1] = votes[0][1];
+    msg_votes.data[2] = votes[0][2];
+    msg_votes.data[3] = votes[1][0];
+    msg_votes.data[4] = votes[1][1];
+    msg_votes.data[5] = votes[1][2];
+    pub_votes.publish(msg_votes);
 }
 
 int main(int argc, char** argv){
@@ -74,22 +91,24 @@ int main(int argc, char** argv){
     ros::NodeHandle n;
     ros::Subscriber sub = n.subscribe("/hardware/realsense/points", 10, callback_pointcloud);
     ros::Publisher pub =  n.advertise<std_msgs::Float64MultiArray>("/hardware/head/goal_pose", 10);
-    ros::Rate loop(1);
+    pub_votes=n.advertise<std_msgs::Int32MultiArray>("/votes", 10);
+    ros::Rate loop(20);
     tf_listener = new tf::TransformListener();
     std_msgs::Float64MultiArray msg_head;
     msg_head.data.resize(2);
     msg_head.data[0] = 0;
-    msg_head.data[1] = -1.3;
+    msg_head.data[1] = -1.2;
     pub.publish(msg_head);
     while(ros::ok()){
-        pub.publish(pc_msg);
         ros::spinOnce();
         loop.sleep();
-        if(info==0)
+        if(info==0){
+            std::cout<<"Moviendo cámara"<<std::endl;
             pub.publish(msg_head);
-            continue;
+        }
+            //continue;
         //cv::imshow("Visualizar",pc_dest);
-        if(cv::waitKey(3)==27)
-            break;
+        //if(cv::waitKey(3)==27)
+            //break;
     }
 };
