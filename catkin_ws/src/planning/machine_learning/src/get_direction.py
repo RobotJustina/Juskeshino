483887eee76c8d645903c35d966b9eccc64828cd
sergioar3143 <sergioar3143@github.com>
2023-04-05 20:05:56 -0600

    The node get_direction now can print the direction in which the robot has to move

--- catkin_ws/src/planning/machine_learning/src/get_direction.py
+++ catkin_ws/src/planning/machine_learning/src/get_direction.py
@@ -7,7 +7,11 @@ import math
 from geometry_msgs.msg import PointStamped
 
 def callback_global_goal(msg):
-    print("Calculating path from robot pose to " + str([msg.point.x, msg.point.y]))
+    global goal_x
+    global goal_y
+    goal_x=msg.point.x
+    goal_y=msg.point.y
+    #print("Calculating path from robot pose to " + str([msg.point.x, msg.point.y]))
     #[robot_x, robot_y, robot_a] = get_robot_pose(listener)
 
 def get_robot_pose(listener):
@@ -22,14 +26,33 @@ def get_robot_pose(listener):
 
 def main():
     global listener
+    global goal_x
+    global goal_y
+    goal_y=0
+    goal_x=0
     rospy.init_node("get_direction")
     rospy.Subscriber('/clicked_point', PointStamped, callback_global_goal)
     listener = tf.TransformListener()
-    loop = rospy.Rate(2)
+    loop = rospy.Rate(0.1)
     #rospy.spin()
     while( not rospy.is_shutdown()):
         [robot_x, robot_y, robot_a]    = get_robot_pose(listener)
-        print(str(robot_x) +" "+ str(robot_y)+" "+str(robot_a))
+        ang_pos=math.atan2(goal_y-robot_y, goal_x-robot_x)
+        if ang_pos > math.pi:
+            ang_pos=ang_pos-2*math.pi
+        ang=ang_pos-robot_a
+        if ang > math.pi:
+            ang=ang-2*math.pi
+        if (ang>=0 and ang< math.pi/4.0) or (ang<0 and ang > -math.pi/4.0):
+            print("Al frente")
+        elif(ang>=math.pi/4.0 and ang< 3*math.pi/4.0):
+            print("Izquierda")
+        elif(ang>=3*math.pi/4.0 and ang<= math.pi) or (ang>=-math.pi and ang <= -3*math.pi/4.0):
+            print("Atras")
+        else:
+            print("Derecha")
+        #print(str(robot_x) +" "+ str(robot_y)+" "+str(robot_a)+" "+str(ang))
+        #print(ang)
         loop.sleep()
 
 if __name__ == '__main__':
