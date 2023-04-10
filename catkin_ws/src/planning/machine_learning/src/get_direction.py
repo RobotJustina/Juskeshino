ea061fee0e97a51379a96b853d0edc78d27c5be8
sergioar3143 <sergioar3143@github.com>
2023-04-09 19:55:16 -0600

    The robot waits before it publishes the direction due to a strange error

--- catkin_ws/src/planning/machine_learning/src/get_direction.py
+++ catkin_ws/src/planning/machine_learning/src/get_direction.py
@@ -7,18 +7,16 @@ import math
 from std_msgs.msg import Int32
 from geometry_msgs.msg import PointStamped
 from actionlib_msgs.msg import GoalStatus
+from std_msgs.msg import Bool
 
 def callback_global_goal(msg):
     global goal_x
     global goal_y
-    #global pub_off
-    global msg_offset
 
     goal_x=msg.point.x
     goal_y=msg.point.y
-    offset=set_offset()
-    #msg_offset=Int32()
-    msg_offset.data=int(offset)
+    #offset=set_offset()
+    #msg_offset.data=int(offset)
 
     #pub_off.publish(msg_offset)
 
@@ -41,50 +39,47 @@ def set_offset():
     global goal_y
     [robot_x, robot_y, robot_a]    = get_robot_pose(listener)
     ang_pos=math.atan2(goal_y-robot_y, goal_x-robot_x)
+    d=math.sqrt( (goal_y-robot_y)**2 + (goal_x-robot_x)**2 )
     if ang_pos > math.pi:
         ang_pos=ang_pos-2*math.pi
     ang=ang_pos-robot_a
     if ang > math.pi:
         ang=ang-2*math.pi
     if (ang>=0 and ang< math.pi/4.0) or (ang<0 and ang > -math.pi/4.0):
-        print("Al frente")
+        #print("Al frente")
         offset=0
     elif(ang>=math.pi/4.0 and ang< 3*math.pi/4.0):
-        print("Izquierda")
+        #print("Izquierda")
         offset=32
     elif(ang>=3*math.pi/4.0 and ang<= math.pi) or (ang>=-math.pi and ang <= -3*math.pi/4.0):
-        print("Atras")
+        #print("Atras")
         offset=64
     else:
-        print("Derecha")
+        #print("Derecha")
         offset=96
+    if(d<0.2):
+        offset+=500
+    print(d)
     return offset
-
-#def callback_goal(msg):
- #   global pub_off
-  #  if(msg.status==3):
-   #     offset=set_offset()
-    #    msg_offset=Int32()
-     #   msg_offset.data=int(offset)
-      #  pub_off.publish(msg_offset)
 
 def main():
     global listener
     global goal_x
     global goal_y
-    global msg_offset
+    #global msg_offset
     msg_offset=Int32()
-    #global pub_off
-    goal_y=0
-    goal_x=0
+    #goal_y=0
+    #goal_x=0
     rospy.init_node("get_direction")
     rospy.Subscriber('/clicked_point', PointStamped, callback_global_goal)
     #rospy.Subscriber("/simple_move/goal_reached", GoalStatus, callback_goal);
     pub_off = rospy.Publisher("/offset", Int32, queue_size=10)
-    listener = tf.TransformListener()
     loop = rospy.Rate(10)
-    #rospy.spin()
-    while( not rospy.is_shutdown()):
+    listener = tf.TransformListener()
+    for i in range(5):
+        loop.sleep()
+    [goal_x, goal_y, temp] = get_robot_pose(listener)
+    while(not rospy.is_shutdown()):
         #set_offset()
         #print(str(robot_x) +" "+ str(robot_y)+" "+str(robot_a)+" "+str(ang))
         #print(ang)
