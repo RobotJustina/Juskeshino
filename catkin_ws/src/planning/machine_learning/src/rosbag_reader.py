a3ab030fb9f5245630152a3b586b6ebcdfe530e5
sergioar3143 <sergioar98@hotmail.com>
2023-09-05 20:19:13 -0600

    Rosgad reader makes csv file

--- catkin_ws/src/planning/machine_learning/src/rosbag_reader.py
+++ catkin_ws/src/planning/machine_learning/src/rosbag_reader.py
@@ -1,14 +1,29 @@
 #! /usr/bin/env python3
 import rospy
 import rosbag
+import numpy as np
+import matplotlib.pyplot as plt
 
 def rosbag_read():
     print("funciona")
-    bag = rosbag.Bag('test.bag')
-    for topic, msg, t in bag.read_messages(topics=['/cmd_vel']):
-        print(msg)
-    bag.close()
+    cmd_list=[]
+    files=['cmd_vel_2.bag', 'cmd_vel.bag']
+    for file in files:
+        bag = rosbag.Bag(file)
+        for topic, msg, t in bag.read_messages(topics=['/hardware/mobile_base/cmd_vel']):
+            cmd_list.append([msg.linear.x, msg.angular.z])
+        bag.close()
+    cmd_numpy=np.array(cmd_list)
 
+    np.savetxt('Velocidades.csv', cmd_numpy, delimiter=',')
+    cmd_numpy=np.transpose(cmd_numpy)
+    print(cmd_numpy.shape)
+
+    #plt.scatter(cmd_numpy[0,:], cmd_numpy[1,:], label="Datos", color="blue")
+    #plt.ylabel('Velocidad angular')
+    #plt.xlabel('Velocidad lineal')
+    #plt.legend()
+    #plt.show()
 
 if __name__ == '__main__':
     try:
