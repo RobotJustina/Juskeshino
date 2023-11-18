d2405b6697302f3d684c7e953c6288a875edbf2e
sergioar3143 <sergioar98@hotmail.com>
2023-11-17 21:11:10 -0600

    laser sim added in training.launch, potential fields changed

--- catkin_ws/src/planning/machine_learning/src/sim_laser.cpp
+++ catkin_ws/src/planning/machine_learning/src/sim_laser.cpp
@@ -27,9 +27,7 @@ int main(int argc, char** argv)
     ros::Rate loop(10);
     ros::Subscriber sub_occ      = n.subscribe("/local_occ_grid", 1, callback_occ);
     ros::Publisher  pub_laser = n.advertise<sensor_msgs::LaserScan>("/laser_mod", 10);
-    tf::TransformListener listener;
-    //tf::TransformBroadcaster broadcaster;
-    //tf::StampedTransform t;
+    //tf::TransformListener listener;
     sensor_msgs::LaserScan real_scan;
     sensor_msgs::LaserScan real_sensor_info;
 
@@ -37,27 +35,18 @@ int main(int argc, char** argv)
     real_sensor_info = real_scan;
     while(ros::ok())
     {
-        try{
-            tf::StampedTransform t;
-            listener.lookupTransform("odom", "base_link", ros::Time(0), t);
-            robot_pose.position.x = 0.1;//t.getOrigin().x();
-            robot_pose.position.y = 0;//t.getOrigin().y();
-            robot_pose.orientation.x = 0;//t.getRotation().x();
-            robot_pose.orientation.y = 0;//t.getRotation().y();
-            robot_pose.orientation.z = 0;//t.getRotation().z();
-            robot_pose.orientation.w = 1;//t.getRotation().w();
-        }
-        catch(std::exception &e){
-            robot_pose.position.x = 0;
-            robot_pose.position.y = 0;
-            robot_pose.orientation.w = 1;
-        }
+        //try{
+            //tf::StampedTransform t;
+            //listener.lookupTransform("odom", "base_link", ros::Time(0), t);
+        robot_pose.position.x = 0.17;//t.getOrigin().x();
+        robot_pose.orientation.w = 1;//t.getRotation().w();
+        //}
+        //catch(std::exception &e){
+        //    robot_pose.orientation.w = 1;
+        //}
         if(map_catch){
-            //simulated_scan.range_max=12;
             simulated_scan = *occupancy_grid_utils::simulateRangeScan(map, robot_pose, real_sensor_info);
             simulated_scan.header.stamp=ros::Time(0);
-            //for(int i=0; i<150; i++)
-            //    simulated_scan.ranges[i]/=14;
             pub_laser.publish(simulated_scan);
         }
         ros::spinOnce();
