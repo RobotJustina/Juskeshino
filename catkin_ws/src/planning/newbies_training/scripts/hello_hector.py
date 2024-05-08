930cb006dad8401f11dba85c144100cdf3576fb9
Lap Jusina <generic@generic.com>
2024-05-07 20:55:52 -0600

    added newbies package

---
+++ catkin_ws/src/planning/newbies_training/scripts/hello_hector.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import rospy
+import rospkg
+from juskeshino_tools.JuskeshinoNavigation import JuskeshinoNavigation
+from juskeshino_tools.JuskeshinoVision import JuskeshinoVision
+from juskeshino_tools.JuskeshinoHardware import JuskeshinoHardware
+from juskeshino_tools.JuskeshinoSimpleTasks import JuskeshinoSimpleTasks
+from juskeshino_tools.JuskeshinoHRI import JuskeshinoHRI
+from juskeshino_tools.JuskeshinoManipulation import JuskeshinoManipulation
+from juskeshino_tools.JuskeshinoKnowledge import JuskeshinoKnowledge
+
+def main():
+    rospy.init_node("hello_hector")
+    rate = rospy.Rate(10)
+    rospack = rospkg.RosPack()
+    JuskeshinoNavigation.setNodeHandle()
+    JuskeshinoVision.setNodeHandle()
+    JuskeshinoHardware.setNodeHandle()
+    JuskeshinoSimpleTasks.setNodeHandle()
+    JuskeshinoHRI.setNodeHandle()
+    JuskeshinoManipulation.setNodeHandle()
+    JuskeshinoKnowledge.setNodeHandle()
+
+    JuskeshinoHRI.say("hello")
+
+if __name__ == "__main__":
+    main()
