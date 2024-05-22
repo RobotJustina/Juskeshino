142c5f90748b7bcdd31acbac65de43113616c364
Lap Jusina <generic@generic.com>
2024-05-21 20:31:34 -0600

    New state, missing recognize objects in state machine (execute already done)

--- catkin_ws/src/planning/newbies_training/scripts/storing_groceries.py
+++ catkin_ws/src/planning/newbies_training/scripts/storing_groceries.py
@@ -38,7 +38,7 @@ class Wait_for_the_door(smach.State):
         if self.counter==1:
             rospy.logwarn('\n--> STATE 1 <: Wait for the door opened')
 
-            JuskeshinoHRI.say("Door is closed. Waiting for door to open")
+            #JuskeshinoHRI.say("Door is closed. Waiting for door to open")
             print("Door is closed. Waiting for door to open")
             rospy.sleep(2.5)
 
@@ -47,27 +47,47 @@ class Wait_for_the_door(smach.State):
         if numpy.mean(msg.ranges[(int)(len(msg.ranges)*0.5 - 10):(int)(len(msg.ranges)*0.5 + 10)]) < 1.0:
             JuskeshinoHRI.say("The door is closed")
             print("The door is closed")
+            self.counter = 0
             return 'failed'
-        else:
+        if numpy.mean(msg.ranges[(int)(len(msg.ranges)*0.5 - 10):(int)(len(msg.ranges)*0.5 + 10)]) > 1.0:
             JuskeshinoHRI.say("The door is opened")
             print("The door is opened")
             return 'succed'
-        self.counter = 0
 
 class Navigate_to_table(smach.State):
     def __init__(self):
-            smach.State.__init__(self, 
-                                outcomes=['succed', 'failed'])
-            self.counter = 0
+        smach.State.__init__(self, 
+                            outcomes=['succed', 'failed'])
+        self.counter = 0
 
         # The input and output data of a state is called 'userdata'
-    def execute(self):
+    def execute(self,userdata):
         self.counter += 1
         if self.counter==1:
             rospy.logwarn('\n--> STATE 2 <: Reaching the table')
             JuskeshinoHRI.say(" I am moving to the table")
+            JuskeshinoNavigation.getClose(location, 300)
             #JuskeshinoNavigation.
-           
+class Find_table(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, 
+                            outcomes=['succed', 'failed'])
+        self.counter = 0
+
+        # The input and output data of a state is called 'userdata'
+    def execute(self,userdata):
+        self.counter += 1
+        if self.counter==1:
+            #Findin bounding table           
+            response = JuskeshinoVision.findTableEdge()
+            if response is None:
+                JuskeshinoHRI.say("Ready for the next step")
+                print("Cannot find the table")
+                self.counter = 0
+                return 'failed'
+            else:
+                print(response)
+                return'succed'
 
 def main():
     rospy.init_node("storing_groceries")
@@ -76,8 +96,7 @@ def main():
     JuskeshinoHardware.setNodeHandle()
     JuskeshinoVision.setNodeHandle()
     JuskeshinoSimpleTasks.setNodeHandle()
-    # JuskeshinoManipulation.setNodeHandle()
-    # print("Continue")
+    JuskeshinoManipulation.setNodeHandle()
     JuskeshinoHRI.setNodeHandle()
     JuskeshinoKnowledge.setNodeHandle()
 
@@ -101,11 +120,30 @@ def main():
                      'succed':'NAVIGATE_TO_TABLE'})
         smach.StateMachine.add('NAVIGATE_TO_TABLE', Navigate_to_table(), 
         transitions={'failed':'NAVIGATE_TO_TABLE', 
-                     'succed':'END'})                     
+                     'succed':'FIND_TABLE'})
+        smach.StateMachine.add('FIND_TABLE',Find_table(), 
+        transitions={'failed':'FIND_TABLE', 
+                     'succed':'END'})                 
         
   
     # Execute SMACH plan
     outcome = sm.execute()
+
+    recog_objects, img = JuskeshinoVision.detectAndRecognizeObjects()
+    for obj in recog_objects:
+        print(obj.id)
+        print(obj.pose.position)
+        print(obj.header.frame_id)
+        x,y,z = obj.pose.position.x, obj.pose.position.y, obj.pose.position.z
+        x,y,z = JuskeshinoSimpleTasks.transformPoint(x,y,z, "shoulders_left_link", "base_link")
+        print(x,y,z)
+        PREPARE_TOP_GRIP  = [-1.25, 0.3, 0, 2.4, 0, 0.7,0]
+        JuskeshinoHardware.moveLeftArmWithTrajectory(PREPARE_TOP_GRIP, 10)  # prepare 
+        response=JuskeshinoManipulation.laIk([x,y,z,0,-1.5,0])
+
+        #[response, success] = JuskeshinoManipulation.GripLa(obj)
+        print(response)
+    
 if __name__ == "__main__":
     main()
 
