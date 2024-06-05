70906876c4e24859c089586cabaab94eced029af
Andres Lopez <lopezesquivelandres1@gmail.com>
2024-06-05 13:21:48 -0600

    The robot approaches the april tag

--- catkin_ws/src/vision/fiducial_markers/scripts/approach_april_tag.py
+++ catkin_ws/src/vision/fiducial_markers/scripts/approach_april_tag.py
@@ -5,24 +5,57 @@ from geometry_msgs.msg import Twist
 from std_msgs.msg import Float32MultiArray, Int8
 
 SAFE_DIST = 1
+moving = False
+moving_angular = True
 
-def move_base(linear, angular, t):
-    rate = rospy.Rate(1/t)
+def move_base(linear, angular):
     cmd = Twist()
     cmd.linear.x = linear
     cmd.angular.z = angular
     pub_cmd_vel.publish(cmd)
-    rate.sleep()
-    pub_cmd_vel.publish(Twist())
 
 def callback_april_tag(msg):
+    global moving
     dist, angle = msg.data
-    if (angle > 0.1) or (angle < -0.1):
+    if (angle > 0.05) or (angle < -0.05):
+        print(f"Moving angularly: {msg.data}")
+        moving_angular = True
         sign = 1 if angle < 0 else -1
-        move_base(0, sign * 0.2, 0.1)
+        move_base(0, sign * 0.1)
     else:
-        if dist > SAFE_DIST:
-            move_base(0.5, 0, 0.1)
+        print("Stopping angular movement")
+        moving_angular = False
+        pub_cmd_vel.publish(Twist())
+
+    if moving_angular:
+        return None
+
+    if dist > SAFE_DIST:
+        print(f"Moving linearly: {msg.data}")
+        move_base(0.1, 0)
+    else:
+        print("Stopping linear movement")
+        pub_cmd_vel.publish(Twist())
+    
+    print() 
+
+    # global moving_angular
+    # global moving
+
+    # if moving:
+    #     return None
+
+    # dist, angle = msg.data
+    # if ((angle > 0.1) or (angle < -0.1)) and moving_angular:
+    #     sign = 1 if angle < 0 else -1
+    #     moving = True
+    #     move_base(0, sign * 0.03, 0.1)
+    #     moving = False
+    # else:
+    #     moving_angular = False
+    #     print(f"Stop moving at angular: {angle}")
+    #     # if dist > SAFE_DIST:
+    #     #     move_base(0.1, 0, 0.1)
 
 # def callback_approach(msg):
 #     move_base(0.1, 0, 1)
