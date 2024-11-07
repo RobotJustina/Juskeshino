3f6a8698f609cc067648bb40db71395ac1b22c88
Daniel_V <ing.danielvanegas@gmail.com>
2024-11-07 11:59:42 -0600

    checkpoint

--- catkin_ws/src/mapless_navigation/create_dataset/scripts/justina_occgrid_data.py
+++ catkin_ws/src/mapless_navigation/create_dataset/scripts/justina_occgrid_data.py
@@ -24,16 +24,11 @@ package_path = rospkg.RosPack().get_path("mapless_nav")
 # model.to(disp)
 lin_vel_x, goal_x, goal_y = 0.0, 0.0, 0.0
 ang_vel_z = 1.0
-last_goal = [0, 0]
-data_X = 0
+last_goal = [0.0, 0.0]
+data_X = []
+data_Y = []
 
 
-def goalCallback(msg):
-    global last_goal
-    last_goal = list(msg.data)
-    #print(last_goal)
-
-    
 def clickPointCallback(msg):
     global goal_x, goal_y
     goal_x = msg.point.x
@@ -41,19 +36,19 @@ def clickPointCallback(msg):
     print(f"\nNew goal ({goal_x:.3f}, {goal_y:.3f})")
 
 
-def target_direction():
+def get_position():
     global listener
-    global goal_x
-    global goal_y
-
     ([x, y, z], rot) = listener.lookupTransform("odom", 'base_link', rospy.Time(0))
     angle = 2*math.atan2(rot[2], rot[3])
     angle = angle - 2*math.pi if angle > math.pi else angle
-    robot_x = x
-    robot_y = y
-    robot_a = angle
+    return x, y, angle
 
 
+def target_direction():
+    global goal_x
+    global goal_y
+    
+    robot_x, robot_y, robot_a = get_position()
     ang_pos = math.atan2(goal_y-robot_y, goal_x-robot_x)
     distance = math.sqrt((goal_y-robot_y)**2 + (goal_x-robot_x)**2)
     if ang_pos > math.pi:
@@ -68,22 +63,20 @@ def target_direction():
     return [distance, angle]
 
 
-def occGridCallback(msg):#SingleConvModel()
+def occGridCallback(msg):
     global data_X
     data = np.asarray(msg.data)
     data = np.reshape(data, (msg.info.height, msg.info.width))
-
-    
     other_features = np.zeros((msg.info.height, msg.info.width))
     other_features[0, :2] = target_direction()
-    data_X = np.array([data, other_features], dtype=np.float32) # img(80x80) ch0, vet(2) ch2
+    # img(80x80) ch0, vet(2) ch2
+    data_X = np.array([data, other_features], dtype=np.float32)
 
-    # print("occ_g:", data.shape)
-    # print("data_X:", data_X.shape)
-    # print(data_X)
-    # print("___________________________________________")
-    # print("___________________________________________")
 
+def cmdVelCallback(msg):
+    global data_Y
+    data_Y = [msg.linear.x, msg.angular.z]
+    
 
 def main():
     global lin_vel_x, ang_vel_z, listener
@@ -95,41 +88,43 @@ def main():
     listener = tf.TransformListener()
     listener.waitForTransform("odom", "base_link", rospy.Time(), rospy.Duration(4.0))
 
-    #rospy.Subscriber("/local_occ_grid_array", Float32MultiArray, occGridArrayCallback)
-    rospy.Subscriber("/NN_goal", Float32MultiArray, goalCallback)
     rospy.Subscriber("/clicked_point", PointStamped, clickPointCallback)
+    rospy.Subscriber("/cmd_vel", Twist, cmdVelCallback)
+    rospy.Subscriber("/local_occ_grid", OccupancyGrid, occGridCallback)
     goal_pub = rospy.Publisher("/NN_goal", Float32MultiArray, queue_size=10)
     msg_pos = Float32MultiArray()
     
     if start:
-        ([x, y, z], rot) = listener.lookupTransform("odom", 'base_link', rospy.Time(0))
-        goal_x, goal_y = x+.001, y+0.1
+        ([x, y, _], _) = listener.lookupTransform("odom", 'base_link', rospy.Time(0))
+        goal_x, goal_y = x, y-0.1
         start = False
 
-    msg_pos.data = last_goal
+    msg_pos.data = target_direction()
     goal_pub.publish(msg_pos)
-    
-    rospy.Subscriber("/local_occ_grid", OccupancyGrid, occGridCallback)
 
-    cmd_vel_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
-    twist_msg = Twist()
-    
     
     loop = rospy.Rate(20)
     while not rospy.is_shutdown():
+        # TODO: Delete
         # cmd_vel_pub.publish(twist_msg)
         # twist_msg.linear.x = lin_vel_x
         # twist_msg.angular.z = ang_vel_z
         # cmd_vel_pub.publish(twist_msg)
+        msg_pos.data = target_direction()
         goal_pub.publish(msg_pos)
-        d, th= target_direction()
-        print(f"(Distancia, Angulo)= ({d:.3f}, {th:.3f})", end='\r')
-        
-        # twist_msg.linear.x = 0.0
-        # twist_msg.angular.z = 0.0
-        # cmd_vel_pub.publish(twist_msg)
+        d, th = target_direction()
+        #print(f"(Distancia, Angulo)= ({d:.3f}, {th:.3f})", end='\r')
+        x, y, a = get_position()
+        #print(f"(Distancia, Angulo)= ({x:.3f}, {y:.3f}, {a:.3f})", end='\r')
 
-        loop.sleep     
+        str = f"(Distancia, Angulo)= ({d:.3f}, {th:.3f})"
+        str += f" || Posicion actual = ({x:.3f}, {y:.3f}, {a:.3f})"
+        #print(str, end='\r')
+
+
+        loop.sleep
+    
+
 
 if __name__ == "__main__":
      main()
