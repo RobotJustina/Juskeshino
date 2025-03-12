3e4fba1b75b19c10d2562a2f4969552d63bbe07e
Itzel <biddygonzalez@gmail.com>
2025-03-11 20:30:09 -0600

    se agregaron grasp de jamon en lata

--- catkin_ws/src/manipulation/object_manipulation/scripts/pose_calculator.py
+++ catkin_ws/src/manipulation/object_manipulation/scripts/pose_calculator.py
@@ -40,7 +40,7 @@ def serialize_msg(msg):
 def main():
     global pc2, hd, obj_pos, grasp_traj, obj_shape, status, trajectory_found, get_object_relative_pose
     obj_pose = Pose()
-    obj_shape = '013_apple'
+    obj_shape = '010_potted_meat_can'
     print("Starting pose calculator node")
     rospy.init_node("pose_calc_node")
     #rospy.wait_for_service("gazebo/spawn_sdf_model")
@@ -65,7 +65,7 @@ def main():
                 transform.child_frame_id = 'relative_grasp'
                 #print(transform)
                 rgbr.sendTransform(transform)
-                gr_pose = get_object_relative_pose("justina_gripper","013_apple").pose
+                gr_pose = get_object_relative_pose("justina_gripper","010_potted_meat_can").pose
                 broadcaster_frame_object('object_frame','grasp_spawn',gr_pose)
                 gripper_model_state.pose = gr_pose
                 print(gripper_model_state)
