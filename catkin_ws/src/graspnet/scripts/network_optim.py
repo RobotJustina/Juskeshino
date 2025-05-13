e6a34ed1464e83788982424206ea85c8e9158f48
Laptop <>
2025-05-13 15:25:59 -0600

    Ori network baysian optimization

--- catkin_ws/src/graspnet/scripts/network_optim.py
+++ catkin_ws/src/graspnet/scripts/network_optim.py
@@ -8,7 +8,7 @@ import geotorch as geo
 import numpy as np
 import copy
 import numpy.lib.recfunctions as rf
-import ros_numpy
+#import ros_numpy
 from geometry_msgs.msg import Point
 from scipy.spatial import cKDTree
 import math
@@ -21,281 +21,199 @@ import numpy.lib.recfunctions as rf
 import gc
 import matplotlib.pyplot as plt
 import math
+import geomstats.backend as gs
+from geomstats.geometry.hypersphere import Hypersphere
 from sklearn.model_selection import train_test_split
 from torch.utils.data import Subset
-from train import get_dataloaders
+from cnn_autoencoder import Encoder
+from ori_network3 import get_SQL_dataloaders, find_kmeans_centers
+from kernel_mixture_network import Kernel_Mixture_Network
 
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-MAX_TRIAL_EPOCHS = 32
-PRUNING_EPOCHS = 8
-BATCH_SIZE = 100
+MAX_TRIAL_EPOCHS = 20
+PRUNING_EPOCHS = 2
+BATCH_SIZE = 192
 DATASET_PATH = "/home/robocup/Juskeshino/catkin_ws/src/graspnet/training_dataset/"
 MODELS_PATH = "/home/robocup/Juskeshino/catkin_ws/src/graspnet/models/"
+BEST_MODEL_PATH = "/home/robocup/Juskeshino/catkin_ws/src/graspnet/models/best_ori_model.pt"
 GRAPHS_PATH = "/home/robocup/Juskeshino/catkin_ws/src/graspnet/graphs/"
 VAL_DATASET_PATH = "/home/robocup/Juskeshino/catkin_ws/src/graspnet/validate_dataset/"
 VAL_TO_TEST_RATIO = 0.1
 FULL_DATASET = -1
-ACTIVATION_FUNC_DICT = {'gelu':nn.GELU,'relu':nn.ReLU}
+#ACTIVATION_FUNC_DICT = {'gelu':nn.GELU,'relu':nn.ReLU}
 #OPTIMIZER_DICT = {'SGD':optim.sgd,'AdamW':optim.adamw}
 #Network definition
 
-class ResidualBlock(nn.Module):
-        def __init__(self, in_channels, out_channels, stride = 1, downsample = None):
-            super(ResidualBlock, self).__init__()
-            self.conv1 = nn.Sequential(
-                            nn.Conv2d(in_channels, out_channels, kernel_size = 3, stride = stride, padding = 1),
-                            nn.BatchNorm2d(out_channels),
-                            nn.ReLU())
-            self.conv2 = nn.Sequential(
-                            nn.Conv2d(out_channels, out_channels, kernel_size = 3, stride = 1, padding = 1),
-                            nn.BatchNorm2d(out_channels))
-            self.downsample = downsample
-            self.relu = nn.ReLU()
-            self.out_channels = out_channels
-
-        def forward(self, x):
-            residual = x
-            out = self.conv1(x)
-            out = self.conv2(out)
-            if self.downsample:
-                residual = self.downsample(x)
-            out += residual
-            out = self.relu(out)
-            return out
-
-class ResidualGraspNetwork(nn.Module):
+class Orientation_head(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Sequential(
-                            nn.Conv2d(6, 32, kernel_size = 3, stride = 1),
-                            nn.BatchNorm2d(32),
-                            nn.ReLU())
-        self.maxpool = nn.MaxPool2d(kernel_size = 3, stride = 2, padding = 1)
-        self.reslayer1 = self._make_layer(block, 64, layers[0], stride = 1)
-        self.reslayer2 = self._make_layer(block, 128, layers[1], stride = 2)
-        self.reslayer3 = self._make_layer(block, 256, layers[2], stride = 2)
-        self.reslayer4 = self._make_layer(block, 512, layers[3], stride = 2)
-        self.fc1 = nn.Linear(512*3*3,1024)
-        self.fc2 = nn.Linear(1024,128)
-        self.fc3 = nn.Linear(128,6)
-        
-    def _make_residual_layer(self, block, planes, blocks, stride=1):
-            downsample = None
-            if stride != 1 or self.inplanes != planes:
-                downsample = nn.Sequential(
-                    nn.Conv2d(self.inplanes, planes, kernel_size=1, stride=stride),
-                    nn.BatchNorm2d(planes),
-                )
-            layers = []
-            layers.append(block(self.inplanes, planes, stride, downsample))
-            self.inplanes = planes
-            for i in range(1, blocks):
-                layers.append(block(self.inplanes, planes))
+        kcenters = torch.load(MODELS_PATH + 'kcenters_256.pt',weights_only=True)
+        self.kmm = Kernel_Mixture_Network(1027,256,len(kcenters),kcenters,4,320)
+        self.space = Hypersphere(3)
+        self.BASE_POINT = torch.tensor([0.0,0.0,0.0,1.0],dtype=torch.float64).to(DEVICE)
+        #print(self)
 
-            return nn.Sequential(*layers)
+    def forward(self, x, pos):
+        x = torch.cat((x,pos),dim=1)
+        ori = self.kmm.sample(x)
+        return ori
+    
+    def L1_loss(self, q1, q2):
+        d = torch.abs(self.space.metric.dist(q1,q2)).mean()
+        return d
+    
+    def L2_loss(self, q1,q2):
+        d = torch.square(self.space.metric.dist(q1,q2)).mean()
+        return d
+    
+    def prob_loss(self, x, y):
+        return self.kmm.loss(x,y)
+    
 
-class GraspNetwork(nn.Module):
-    def __init__(self,trial):
+class Orientation_network(nn.Module):
+    def __init__(self, trial, enc_state_dict = None, kcenters = None):
         super().__init__()
-        #conv1_af = trial.suggest_categorical('C1_ActF',['gelu','relu'])
-        flat1 = trial.suggest_int("cnn_out_neurons{}".format(1), 4096,8192)
-        fkernels = trial.suggest_int("cnn_out_kernels{}".format(1), 128,512)
+        self.enc = Encoder().float()
+        if enc_state_dict:
+            self.enc.load_state_dict(enc_state_dict)
+        if kcenters == None:
+            kcenters = torch.load(MODELS_PATH + 'kcenters_256.pt',weights_only=True)
+            #print(kcenters)
+            #print(kcenters.dtype)
+        print(kcenters)
+        self.kmm = Kernel_Mixture_Network(1027,256,len(kcenters),kcenters,4,trial.suggest_int('kappa',25,500))
+        self.space = Hypersphere(3)
+        self.BASE_POINT = torch.tensor([0.0,0.0,0.0,1.0],dtype=torch.float64).to(DEVICE)
+        #print(self)
 
-        self.conv1 = nn.Sequential(
-                            #nn.Dropout2d(0.05),
-                            nn.Conv2d(3, 32, kernel_size = 11, stride = 1),
-                            nn.BatchNorm2d(32),
-                            #ACTIVATION_FUNC_DICT[conv1_af]()
-                            #nn.GELU()
-                            )
-        self.maxpool = nn.MaxPool2d(kernel_size = 2, stride = 2, padding = 0)
-        self.conv2 = nn.Sequential(
-                            nn.Conv2d(32, 64, kernel_size = 6, stride = 1),
-                            nn.BatchNorm2d(64),
-                            nn.ReLU())
-        self.conv3 = nn.Sequential(
-                            nn.Conv2d(64, 128, kernel_size = 4, stride = 2),
-                            nn.BatchNorm2d(128),
-                            nn.ReLU())
-        self.conv4 = nn.Sequential(
-                            nn.Conv2d(128, 256, kernel_size = 4, stride = 2),
-                            nn.BatchNorm2d(256),
-                            nn.ReLU())
-        self.conv5 = nn.Sequential(
-                            nn.Conv2d(256, 512, kernel_size = 3, stride = 2),
-                            nn.BatchNorm2d(512),
-                            nn.ReLU())
-                            #,nn.Dropout2d(0.1))
-        self.conv6 = nn.Sequential(
-                            nn.Conv2d(512, fkernels, kernel_size = 2, stride = 2, padding=1),
-                            nn.BatchNorm2d(fkernels),
-                            #nn.GELU()
-                            )
-        self.maxpool2 = nn.MaxPool2d(kernel_size=2,stride=1,padding=0)
-        self.lc  = nn.Sequential(
-                            nn.Linear(5*5*fkernels,flat1),
-                            nn.ReLU(),
-                            nn.Dropout(0.5),
-                            nn.Linear(flat1,2048),
-                            # nn.ReLU(),
-                            # nn.Linear(4096, 2048),
-                            nn.ReLU(),
-                            nn.Dropout(0.4),
-                            nn.Linear(2048,512),
-                            nn.ReLU(),
-                            nn.Dropout(0.25),
-                            nn.Linear(512,256),
-                            nn.ReLU(),
-                            nn.Dropout(0.25),
-                            nn.Linear(256,256),
-                            nn.Linear(256,3))
-                            #nn.Linear(64,3))
-        self.nlc = nn.Sequential(
-                            nn.Linear(5*5*fkernels,flat1),
-                            nn.ReLU(),
-                            nn.Dropout(0.5),
-                            nn.Linear(flat1,2048),
-                            # nn.ReLU(),
-                            # nn.Linear(4096, 2048),
-                            nn.ReLU(),
-                            nn.Dropout(0.5),
-                            nn.Linear(2048, 512),
-                            nn.ReLU(),
-                            nn.Dropout(0.4),
-                            nn.Linear(512,512),
-                            nn.ReLU(),
-                            nn.Dropout(0.25),
-                            nn.Linear(512,256),
-                            nn.ReLU(),
-                            nn.Linear(256,4))
-                            # nn.ReLU(),
-                            # nn.Linear(64,4),
-                            #geo.Sphere()
-                            #nn.Tanh())
-        #self.nlch = nn.Linear(256,4)
-        #ten = getattr(self.nlch,)
-        #self.manif = geo.Sphere(ten.size(),1)
-        self.manif = geo.Sphere([1,4],1)
-        self.manif.base = torch.tensor([0,0,0,1],dtype=torch.float32)
-        #self.manif = geo.SphereEmbedded(self.nlc,1)
-        #self.nlch = nn.Linear(256,4)
-        #geo.sphere(self.nlch, tensor_name="output")
-        #geo.SphereEmbedded(self.nlc,1)
-        #geo.orthogonal()
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.maxpool(x)
-        x = F.gelu(x)
-        x = self.conv2(x)
-        x = self.conv3(x)
-        x = self.conv4(x)
-        x = self.conv5(x)
-        x = self.conv6(x)
-        x = self.maxpool2(x)
-        x = F.gelu(x)
-        x = torch.flatten(x,1)
-
-        pos = self.lc(x)
-        ori = self.nlc(x)
-        ori = self.manif(ori)
-
-        return pos, ori
+    def forward(self, x, pos):
+        x = self.enc(x)
+        x = torch.cat((x,pos),dim=1)
+        ori = self.kmm(x)
+        return x, ori
+    
+    def L1_loss(self, q1, q2):
+        d = torch.abs(self.space.metric.dist(q1,q2)).mean()
+        return d
+    
+    def L2_loss(self, q1,q2):
+        d = torch.square(self.space.metric.dist(q1,q2)).mean()
+        return d
+    
+    def prob_loss(self, x, y):
+        return self.kmm.loss(x,y)
+    
+    def predict(self, x):
+        return self.kmm.sample(x)
     
 def train_network(num_epochs, trial, train_loader, valid_loader):
+    global best_model_loss
     train_size = len(train_loader.dataset)
     val_size = len(valid_loader.dataset)
-    model = GraspNetwork(trial).to(DEVICE)
-    #best_model = copy.deepcopy(model.state_dict())
-    criterion = nn.HuberLoss(delta=0.9)
-    exp_error = nn.L1Loss(reduction='mean')
-    min_loss = 15000
-
-    suggested_weight_decay = trial.suggest_float('weight_decay',1e-4,1e-1, log=True)
-    suggested_lr = trial.suggest_float('lr',1e-6,1e-3, log=True)
-    #suggested_optimizer = trial.suggest_categorical('optimizer',['SGD','AdamW'])
-    
-    #optimizer = optim.SGD(model.parameters(),lr=0.00008,momentum=0.8)
-    # if suggested_optimizer == 'SGD':
-    #     suggested_momentum = trial.suggest_float('momentum',0.8,0.95,log=True)
-    #     optimizer = optim.SGD(model.parameters(),lr=suggested_lr,momentum=suggested_momentum,nesterov=True,weight_decay=suggested_weight_decay)
-    
-    #optimizer = optim.Adam(model.parameters(),lr=0.00008)
-    #if suggested_optimizer == 'AdamW':
-    optimizer = optim.AdamW(model.parameters(),lr=suggested_lr,weight_decay=suggested_weight_decay)
-    
+    #cae_path = MODELS_PATH + 'dummy_cae_10ks_ep97_split_dict.pt'
+    cae_path = MODELS_PATH + 'conv_autoencoder_final_ep67.pt'
+    enc_dict = torch.load(cae_path,weights_only=True)['encoder_state_dict']
+    kcenters = torch.load(MODELS_PATH + 'kcenters_256.pt',weights_only=True)
+    model = Orientation_network(trial, enc_dict, kcenters).to(DEVICE)
+    min_loss = best_model_loss
+    suggested_weight_decay = trial.suggest_float('weight_decay',1e-4,1e-1, log=False)
+    suggested_initlr = trial.suggest_float('init_lr',1e-6,1e-5, log=False)
+    suggested_maxlr = trial.suggest_float('max_lr',1e-4,1e-2, log=False)
+    optimizer = optim.AdamW([
+                {'params': model.enc.parameters()},
+                {'params': model.kmm.wi_network.parameters(), 'lr': suggested_initlr}
+            ],lr=suggested_initlr,weight_decay=suggested_weight_decay)
+    #lrscheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer,'min',factor=0.1,patience=3,threshold=)
+    lrscheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer=optimizer,max_lr=suggested_maxlr,steps_per_epoch=len(train_loader),epochs=num_epochs)
     for epoch in range(num_epochs):
         trunning_loss = 0
         vrunning_loss = 0
-        tpos_err = 0
-        vpos_err = 0
-        tori_err = 0
-        vori_err = 0
         model.train()
-        for batch, (points, target_pose) in enumerate(train_loader):
-            points = points.to(DEVICE)
-            target_pose = target_pose.to(DEVICE)
 
-            pos, ori = model(points)
-            ploss = criterion(pos,target_pose[:,:3])
-            oloss = criterion(ori,target_pose[:,3:])
-            tloss = ploss + oloss
+        for batch, (pcd, pos, target_ori) in enumerate(train_loader,0):
+            cpu_ori = target_ori
+            pcd, pos, target_ori = pcd.to(DEVICE, non_blocking = True), pos.to(DEVICE, non_blocking = True), target_ori.to(DEVICE, non_blocking = True)
 
-            optimizer.zero_grad()
-            tloss.backward()
-            optimizer.step()
+            with torch.device(DEVICE):
+                x, wi = model(pcd, pos)
+                #ori = model.predict(x)
+                #print(ori.shape)
+                tloss = model.kmm.loss(x,cpu_ori).mean()
+                #tloss = model.L2_loss(ori,target_ori)
+
+                optimizer.zero_grad()
+                tloss.backward()
+                optimizer.step()
+                lrscheduler.step()
             
-            trunning_loss += tloss.item() * points.shape[0]
-            tpos_err += exp_error(pos,target_pose[:,:3]).item() * points.shape[0]
-            tori_err += exp_error(ori,target_pose[:,3:]).item() * points.shape[0]
+                trunning_loss += tloss.item() * pcd.shape[0]
 
-            #del points, target_pose, output_pose
-            del points, target_pose, pos, ori
-            torch.cuda.empty_cache()
-            gc.collect()
-        
-        model.eval()    
-        with torch.no_grad():
-            for points, target_pose in valid_loader:
-                error = 0
-                points = points.to(DEVICE)
-                target_pose = target_pose.to(DEVICE)
-                pos, ori = model(points)
-                ploss = criterion(pos,target_pose[:,:3])
-                oloss = criterion(ori,target_pose[:,3:])
-                valloss = ploss + oloss
-
-                vrunning_loss += valloss.item() * points.shape[0]
-                vpos_err += exp_error(pos,target_pose[:,:3]).item() * points.shape[0]
-                vori_err += exp_error(ori,target_pose[:,3:]).item() * points.shape[0]
-
-                del points, target_pose, pos, ori
+                del pcd, target_ori, cpu_ori, pos, x
                 torch.cuda.empty_cache()
                 gc.collect()
+
+        #lrscheduler.step()
+
+        model.eval()    
+        with torch.no_grad():
+            for pcd, pos, target_ori in valid_loader:
+                pcd = pcd.to(DEVICE, non_blocking = True)
+                pos = pos.to(DEVICE, non_blocking = True)
+                #cpu_ori = copy.deepcopy(target_ori)
+                cpu_ori = target_ori
+                target_ori = target_ori.to(DEVICE, non_blocking = True)
+                
+                with torch.device(DEVICE):
+                    x, wi = model(pcd, pos)
+                    #ori = model.predict(x)
+                    valloss = model.kmm.loss(x,cpu_ori).mean()
+                    #valloss = model.L2_loss(ori, target_ori)
+
+                    vrunning_loss += valloss.item() * pcd.shape[0]
+                    #vori_err += exp_error(ori,space.metric.exp(target_ori,BASE_POINT)).item() * pcd.shape[0]
+                    #vori_err += exp_error(ori,target_ori).item() * pcd.shape[0]
+
+                    del pcd, target_ori, cpu_ori, pos, x
+                    torch.cuda.empty_cache()
+                    gc.collect()
             if (vrunning_loss / val_size) < min_loss:
+                torch.save({
+                    'encoder_state_dict': model.enc.state_dict(),
+                    'ori_state_dict': model.kmm.state_dict()
+                },BEST_MODEL_PATH)
                 min_loss = vrunning_loss / val_size
-                saved_epoch = epoch + 1
-                #best_model = copy.deepcopy(model.state_dict())       
+                best_model_loss = vrunning_loss / val_size
+        #print ('Epoch [{}/{}], Training Loss: {:.4f}'.format(epoch+1, num_epochs, trunning_loss / train_size))
+        #print ('Epoch [{}/{}], Validation Loss: {:.4f}'.format(epoch+1, num_epochs, vrunning_loss / val_size))
+
         trial.report(vrunning_loss / val_size, epoch)
 
         # Handle pruning based on the intermediate value.
         if epoch > PRUNING_EPOCHS:
             if trial.should_prune():
                 raise optuna.exceptions.TrialPruned()
-        
+            
+    # if min_loss < best_model_loss:
+    #     torch.save({
+    #         'encoder_state_dict': best_enc_model,
+    #         'ori_state_dict': best_ori_model
+    #     },BEST_MODEL_PATH)
+
     print ('Trial number [{}], Validation Loss: {:.8f}'.format(trial.number,min_loss))
     return(min_loss)
 
 def objective(trial):
     torch.cuda.empty_cache()
     gc.collect()
-    train_loader, valid_loader = get_dataloaders(n_samples=4200)
+    train_loader, valid_loader = get_SQL_dataloaders(n_samples=4200)
     trial_loss = train_network(MAX_TRIAL_EPOCHS,trial,train_loader,valid_loader)
     return trial_loss
 
 def main():
-    study_number = 3
-    study_id = "network_optim" + str(study_number) # Unique identifier of the study.
+    global best_model_loss
+    best_model_loss = 10
+    study_number = 1
+    study_id = "ori_network_optim" + str(study_number) # Unique identifier of the study.
     study_storage = "sqlite:///catkin_ws/src/graspnet/{}.db".format(study_id)
     study = optuna.create_study(study_name=study_id,storage=study_storage, direction="minimize")
     study.optimize(objective, n_trials=200, timeout=None)
